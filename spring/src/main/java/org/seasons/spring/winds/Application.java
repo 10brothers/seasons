5a62178bbf046227d0c6c10f121512e4ce2d9935
wangke <wangke@asiainfo-net.com>
2022-02-22 22:28:45 +0800

    init

---
+++ spring/src/main/java/org/seasons/spring/winds/Application.java
@@ -0,0 +1,18 @@
+package org.seasons.spring.winds;
+
+import org.seasons.spring.winds.model.Bird;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * --描述
+ *
+ * @author wangk
+ * @date 2022/2/22
+ */
+public class Application {
+    public static void main (String[] args) {
+        AnnotationConfigApplicationContext aca = new AnnotationConfigApplicationContext();
+
+        aca.register(Bird.class);
+    }
+}
